5e1b7d20b19305b8b822890e23bf972db1d9e34a
FrankBowers24 <FrankBowers24@gmail.com>
2015-04-28 16:43:56 -0700

    Added Tooltip

--- public/force.js
+++ public/force.js
@@ -1,4 +1,35 @@
 //http://bl.ocks.org/mbostock/1062288
+//
+// Early in the code
+var w = $(window).width();
+var h = $(window).height();
+var margin = {
+    x: 10,
+    y: 10
+};
+var padding = {
+    x: 10,
+    y: 10
+};
+var tooltip = {
+    width: $('.tooltip').width(),
+    height: $('.tooltip').height(),
+}
+var scene = {
+    x: margin.x + padding.x,
+    y: margin.y + padding.y,
+    width: w - (margin.x * 2) - (padding.x * 2),
+    height: h - (margin.y * 2) - (padding.y * 2)
+}
+
+var div = d3.select("body")
+  .append("div")
+  .style("position", "absolute")
+  .style("z-index", "10")
+  .style("opacity", "0")
+  .text("a simple tooltip");
+
+/// end tootip code
 
 var width = 1100,
     height = 600,
@@ -81,6 +112,9 @@ function update() {
       // .attr("cy", function(d) { return d.y; })
       // .attr("r", function(d) { return Math.sqrt(d.size) / 10 || 4.5; })
       // .style("fill", color)
+      .on("mouseover", mouseover)
+      .on("mousemove", mousemove)
+      .on("mouseout", mouseout)
       .on("click", click)
       //.on("dblclick", dblClick)
       // .attr("ng-click", "findSimilar()")
@@ -190,4 +224,78 @@ function flatten(root) {
 
   recurse(root);
   return nodes;
+}
+
+
+/*     -------------------    */
+var popupHtml = function(d) {
+  return '<img height="250" src="' + d.LargeImageURL + '"/>';
+}
+
+function positionTooltip(mouse, scene, tooltip)
+{
+    //Distance of element from the right edge of viewport
+    if (scene.width - (mouse.x + tooltip.width) < 20)
+    { //If tooltip exceeds the X coordinate of viewport
+        mouse.x = mouse.x - tooltip.width - 20;
+    }
+    //Distance of element from the bottom of viewport
+    if (scene.height - (mouse.y + tooltip.height) < 20)
+    { //If tooltip exceeds the Y coordinate of viewport
+        mouse.y = mouse.y - tooltip.height; // - 20;
+    }
+    return {
+        top: mouse.y-200,
+        left: mouse.x
+    };
+}
+
+
+
+// then, when defining the tooltip div events...
+
+ 
+var mouseover = function(d) {
+    div.transition().duration(200).style("opacity", 1);
+    //div.html(popupHtml(d)).style("left", (d3.event.pageX + 10) + "px").style("top", (d3.event.pageY - 10) + "px");
+    div.html(popupHtml(d)).style("left", function()
+    {
+        var pos = positionTooltip(
+        {
+            x: d3.event.pageX,
+            y: d3.event.pageY
+        }, scene, tooltip);
+        return (pos.left + 10) + 'px';
+    }).style("top", function()
+    {
+        var pos = positionTooltip(
+        {
+            x: d3.event.pageX,
+            y: d3.event.pageY
+        }, scene, tooltip);
+        return (pos.top - 10) + 'px';
+    });
+}
+var mousemove = function(d) {
+    //div.style("left", (d3.event.pageX + 10) + "px").style("top", (d3.event.pageY - 10) + "px");
+    div.style("left", function()
+    {
+        var pos = positionTooltip(
+        {
+            x: d3.event.pageX,
+            y: d3.event.pageY
+        }, scene, tooltip);
+        return (pos.left + 10) + 'px';
+    }).style("top", function()
+    {
+        var pos = positionTooltip(
+        {
+            x: d3.event.pageX,
+            y: d3.event.pageY
+        }, scene, tooltip);
+        return (pos.top - 10) + 'px';
+    });
+}
+var mouseout = function(d) {
+      div.transition().duration(500).style("opacity", 0);
 }
